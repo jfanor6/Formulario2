f5d72d667c77fca9922d2956f0d25f53641be13b
jfanor6 <98598509+jfanor6@users.noreply.github.com>
2022-01-28 14:55:24 -0500

    Add files via upload

--- con_db.php
+++ con_db.php
@@ -4,8 +4,15 @@
     header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE,OPTIONS');
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-	define("HOSTNAME", "localhost");
+	/*define("HOSTNAME", "localhost");
 	define("DATABASE", "prueba_tecnica_dev");      
+	define("USERNAME", "root");    
+	define("PASSWORD", "");      
+
+	$mysqli = new mysqli(HOSTNAME, USERNAME, PASSWORD, DATABASE);*/
+	
+	define("HOSTNAME", "104.156.58.29");
+	define("DATABASE", "impulsar_prueba_tecnica_dev");      
 	define("USERNAME", "root");    
 	define("PASSWORD", "");      
 
